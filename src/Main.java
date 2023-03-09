59e922cb54b39cc74cc72f6bc7dcd1529f3d196a
Alexandra Yakovleva <al.rmn30@gmail.com>
2023-03-09 15:59:16 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 15_000;
+        int bonusMileCost = 20;
+        int bonusMiles = ticketPrice / bonusMileCost;
+        System.out.println("Количество бонусных миль: " + bonusMiles);
+    }
+}
+
+
+
+
+
+
